94a87671517f6b32e1fa1bb39f5d435ee0544bd0
ainanyfitiagershom <fitiagershom@yahoo.com>
2025-03-10 21:36:30 +0300

    Ajout site vitrine pour les clients

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './views/auth/register/register.component';
 import { LoginComponent } from './views/auth/login/login.component';
+import { FirstViewComponent } from './views/user/first-view/first-view.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { provideRouter } from '@angular/router';
@@ -10,7 +11,9 @@ export const routes: Routes = [
 
     { path: 'login', component: LoginComponent },
 
-    { path: 'register', component: RegisterComponent}
+    { path: 'register', component: RegisterComponent},
+
+    { path: 'first_view_client', component: FirstViewComponent}
 ];
 
 bootstrapApplication(AppComponent, {
---
+++ src/app/views/user/first-view/first-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-view',
+  imports: [],
+  templateUrl: './first-view.component.html',
+  styleUrl: './first-view.component.css'
+})
+export class FirstViewComponent {
+
+}
