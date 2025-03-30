20715167287ae09a00cd7682d2c1cdd98a69fa4c
ainanyfitiagershom <fitiagershom@yahoo.com>
2025-03-30 17:47:23 +0300

    Listes des pieces

--- src/app/pages/ui-components/ajout_pieces/ajout_pieces.component.ts
+++ src/app/pages/ui-components/ajout_pieces/ajout_pieces.component.ts
@@ -8,11 +8,59 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../../material.module';
 
 interface Food {
   value: string;
   viewValue: string;
 }
+
+export interface productsData {
+  id: number;
+  nom: string;
+  nombre: number;
+  prix_unitaire: string;
+  etat: string;
+  progress: string;
+}
+
+
+const ELEMENT_DATA: productsData[] = [
+  {
+    id: 1,
+    nom: 'Frein',
+    prix_unitaire: '3.5',
+    nombre: 73,
+    etat: 'Disponible',
+    progress: 'success',
+  },
+  {
+    id: 2,
+    nom: 'Volant',
+    prix_unitaire: '3.5',
+    nombre: 73,
+    etat: 'Non disponible',
+    progress: 'warning',
+  },
+  {
+    id: 3,
+    nom: 'Vitesse',
+    prix_unitaire: '3.5',
+    nombre: 73,
+    etat: 'Non disponible',
+    progress: 'warning',
+  },
+  {
+    id: 4,
+    nom: 'Ampoule',
+    prix_unitaire: '3.5',
+    nombre: 73,
+    etat: 'Non disponible',
+    progress: 'warning',
+  },
+];
 
 @Component({
   selector: 'app-forms',
@@ -27,11 +75,14 @@ interface Food {
     MatInputModule,
     MatCheckboxModule,
     MatListModule,
+    MaterialModule, 
+    MatMenuModule, 
+    CommonModule
   ],
   templateUrl: './ajout_pieces.component.html',
 })
 export class AppAjoutPiecesComponent {
-  
+
   country: Food[] = [
     { value: 'steak-0', viewValue: 'Toyota' },
     { value: 'pizza-1', viewValue: 'Subaru' },
@@ -40,4 +91,7 @@ export class AppAjoutPiecesComponent {
   ];
 
   selectedCountry = this.country[2].value;
+
+  displayedColumns: string[] = ['assigned', 'nombre', 'etat', 'prix_unitaire'];
+  dataSource = ELEMENT_DATA;
 }
